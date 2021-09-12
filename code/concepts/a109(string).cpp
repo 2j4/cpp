57e736f89f838f1b9c0204aec1808d32622f6c82
jjd <56191796+2j4@users.noreply.github.com>
2021-09-12 22:51:35 +0800

    Update a109(string).cpp

--- code/concepts/a109(string).cpp
+++ code/concepts/a109(string).cpp
@@ -3,12 +3,11 @@
 #define ll long long
 using namespace std;
 const int N = 1000000;
-string s, bin[8]={"000", "001", "010","011","100","101","110","111"}, ans;
+string s, bin[8]={"000", "001", "010", "011", "100", "101", "110", "111"}, ans;
 int n;
 bool er;
 char nw;
-signed main()
-{
+signed main(){
 	ios::sync_with_stdio(0), cin.tie(0);
 	cin >> n;
 	getline(cin, s);
